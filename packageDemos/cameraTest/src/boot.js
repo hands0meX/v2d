f19d61b3393a95ec9697706872801ba88b9c9dc8
handsomeX <1258849260@qq.com>
2022-03-14 20:39:21 +0800

    feat:摄像头操作✅ - PC

--- packageDemos/cameraTest/src/boot.js
+++ packageDemos/cameraTest/src/boot.js
@@ -1,27 +1,11 @@
-// user授权使用媒体
-
-// navigator.mediaDevices.getUserMedia(
-//     {
-//         video:true      //{width: 1280, height: 720}
-//     }
-// )
-// .then(res => {
-//     debugger
-// })
-
-
-
-//可使用的媒体列表
-
-
-
-
-//可以设置展示一个窗口（像小窗口一样的分享到想要的媒体流中）
-// navigator.mediaDevices.getDisplayMedia(
-//     {
-//         video: true
-//     }
-// )
-// .then(res => {
-//     debugger;
-// })
+const video = document.getElementById("video")
+// video.width = window.screen.width
+// video.height = window.screen.height
+navigator.mediaDevices.getUserMedia({ video: {facingMode:"user"} })
+.then( stream => {
+    video.srcObject = stream
+    video.play()
+})
+.catch( err => {
+    debugger;
+})
--- packageDemos/cameraTest/src/test.js
+++ packageDemos/cameraTest/src/test.js
@@ -1,10 +1,27 @@
-const video = document.getElementById("video")
+·// user授权使用媒体
 
-navigator.mediaDevices.getUserMedia({ video: {width: 500, height: 500,facingMode:"user"} })
-.then( stream => {
-    video.srcObject = stream
-    video.play()
-})
-.catch( err => {
-    debugger;
-})
+// navigator.mediaDevices.getUserMedia(
+//     {
+//         video:true      //{width: 1280, height: 720}
+//     }
+// )
+// .then(res => {
+//     debugger
+// })
+
+
+
+//可使用的媒体列表
+
+
+
+
+//可以设置展示一个窗口（像小窗口一样的分享到想要的媒体流中）
+// navigator.mediaDevices.getDisplayMedia(
+//     {
+//         video: true
+//     }
+// )
+// .then(res => {
+//     debugger;
+// })
